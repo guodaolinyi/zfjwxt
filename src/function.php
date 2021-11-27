15bb76a74c6e20952b467e2c336572c3168289c9
锅岛霖懿 <guodaolinyi@qq.com>
2021-11-27 21:22:29 +0800

    移除遍历删除方法

--- src/function.php
+++ src/function.php
@@ -129,35 +129,6 @@ if (!function_exists('captcha_new')) {
     }
 }
 
-/**
- * del_dir() 函数用来删除目录下的所有文件
- */
-function del_dir($path)
-{
-    if (is_dir($path)) {
-        //扫描一个文件夹内的所有文件夹和文件并返回数组
-        $p = scandir($path);
-        foreach ($p as $val) {
-            //排除目录中的.和..
-            if ($val != "." && $val != "..") {
-                //如果是目录则递归子目录，继续操作
-                if (is_dir($path . $val)) {
-                    //子目录中操作删除文件夹和文件
-                    deldir($path . $val . '/');
-                    //目录清空后删除空文件夹
-                    @rmdir($path . $val . '/');
-                } else {
-                    //判断时间
-                    if (fileatime($path . $val) < time() - 3600) {
-                        //如果是文件直接删除
-                        unlink($path . $val);
-                    }
-                }
-            }
-        }
-    }
-}
-
 /*
  * xpath操作
  * */
